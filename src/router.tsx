50ad1e6a90523931956c67e32eb30e7c2b20d871
HarlleyRocha <harlley.rocha@escolar.ifrn.edu.br>
2026-07-29 19:46:32 -0300

    arrumando

--- src/router.tsx
+++ src/router.tsx
@@ -3,7 +3,11 @@ import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      mutations: { networkMode: "always" },
+    },
+  });
 
   const router = createRouter({
     routeTree,
